0a67cadd9fd5ed59178863f3d51b4357f0a2cbab
Michal Kloczko <michal.kloczko@stxnext.pl>
2016-03-09 15:41:38 +0100

    add browserify, sass and jquery

--- gobblefile.js
+++ gobblefile.js
@@ -1,3 +1,29 @@
 var gobble = require('gobble');
 
-module.exports = gobble('src');
+module.exports = gobble([
+    gobble('src/root'),
+    sassifyStyles('src/styles'),
+    browserifyScripts('src/scripts')
+]);
+
+function sassifyStyles(sourceDir) {
+
+    return gobble(sourceDir)
+        .transform('sass', {
+            src: 'main.scss',
+            dest: 'styles/app.css'
+        });
+
+}
+
+function browserifyScripts(sourceDir) {
+
+    return gobble(sourceDir)
+        .transform('browserify', {
+            entries: 'app.js',
+            dest: 'app.js',
+            debug: true
+        })
+        .moveTo('scripts');
+
+}
---
+++ src/scripts/app.js
@@ -0,0 +1,3 @@
+var $ = require('jquery-browserify');
+
+$('header').text('ttt');
